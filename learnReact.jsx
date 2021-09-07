d21d1b76efa3a4367122cedba59dca0622c5f633
TylorEM <TylorEM@users.noreply.github.com>
2021-09-06 20:32:18 -0400

    update

--- learnReact.jsx
+++ learnReact.jsx
@@ -823,3 +823,37 @@ const getPrice = () => {
 }
 
 //------------------------------------------------------
+
+//* Counter
+
+import React, {useState} from 'react';
+import {render} from 'react-dom'
+
+const Counter = () => {
+  const [seconds, setSeconds] = useState(0)
+
+  return <h2>{seconds} seconds elapsed</h2>
+}
+
+render(<Counter />, document.querySelector('#react-root'))
+
+export default Counter;
+
+//------------------------------------------------------
+
+//* Countdown
+
+import React, {useState} from 'react'
+import {render} from 'react-dom'
+
+const Countdown = () => {
+  const [count, setCount] = useState(10)
+
+  return <h2>{count} times remaining</h2>
+}
+
+render(<Countdown />, document.querySelector('#react-root'))
+
+//------------------------------------------------------
+
+
